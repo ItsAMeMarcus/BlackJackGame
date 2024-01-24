c92573b25c9a92eefc8d98d4e31da145e93b2905
ItsAMeMarcus <mrmavicico@gmail.com>
2024-01-24 00:21:33 -0300

    bug do pulo resolvido

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,13 +1,10 @@
 extends CharacterBody2D
 
-
 @export var SPEED = 100.0
 @export var JUMP_VELOCITY = -100.0
-@export var gravity = -200
+var gravity  = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var is_attacking = false
-var can_jump:bool
-
 
 func player_animations():
 	if Input.is_action_pressed("ui_left") || Input.is_action_just_released("ui_jump"):
@@ -22,7 +19,8 @@ func player_animations():
 		
 	if !Input.is_anything_pressed():
 		$AnimatedSprite2D.play("default")
-	
+		
+
 func horizontal_movement():
 	var horizontal_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
@@ -33,35 +31,24 @@ func _input(event):
 		is_attacking = true
 		$AnimatedSprite2D.play("attack")
 		
-	if event.is_action_pressed("ui_jump") and can_jump:
-		velocity.y = JUMP_VELOCITY
-		$AnimatedSprite2D.play("jump")
+func _on_animated_sprite_2d_animation_finished():
+	is_attacking = false # Replace with function body.
 
 #movimento e fisica
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	velocity.y += gravity * delta
 
-	# Handle jump.
-	if is_on_floor() && !Input.is_action_pressed("ui_jump"):
-		can_jump = true
-		
-	#aplica movimento
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		$AnimatedSprite2D.play("jump")
+
+	# Get the input direction.
 	horizontal_movement()
-	
+	#aplica movimento
 	move_and_slide()
-	
 	#aplica animacao
 	if !is_attacking:
 		player_animations()
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 
-	#move_and_slide()
-	
-	#
-
-
-func _on_animated_sprite_2d_animation_finished():
-	is_attacking = false # Replace with function body.
