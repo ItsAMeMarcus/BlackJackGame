24a1a81d016bfaa42981ea7b4112252a5b3b64b5
ItsAMeMarcus <mrmavicico@gmail.com>
2024-01-26 18:39:44 -0300

    bug da animacao dos pulos resolvido

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -8,19 +8,18 @@ var gravity  = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func player_animations():
-	if Input.is_action_pressed("ui_left") || Input.is_action_just_released("ui_jump"):
+	if Input.is_action_pressed("ui_left") and Global.is_jumping == false:
 		$AnimatedSprite2D.flip_h = true
 		$AnimatedSprite2D.play("run")
 		$CollisionShape2D.position.x = -7
 		
-	if Input.is_action_pressed("ui_right") || Input.is_action_just_released("ui_jump"):
+	if Input.is_action_pressed("ui_right") and Global.is_jumping == false:
 		$AnimatedSprite2D.flip_h = false
 		$AnimatedSprite2D.play("run")
 		$CollisionShape2D.position.x = -7
 		
 	if !Input.is_anything_pressed():
 		$AnimatedSprite2D.play("default")
-		
 
 func horizontal_movement():
 	var horizontal_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -31,27 +30,24 @@ func _input(event):
 	if event.is_action_pressed("ui_attack"):
 		Global.is_attacking = true
 		$AnimatedSprite2D.play("attack")
-		
+	if Input.is_action_just_pressed("ui_jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		Global.is_jumping = true
+		$AnimatedSprite2D.play("jump")
+	else:
+		Global.is_jumping = false
+
 func _on_animated_sprite_2d_animation_finished():
 	Global.is_attacking = false
 	Global.is_on_door = false
-# Replace with function body.
 
-#movimento e fisica
 func _physics_process(delta):
 	# Add the gravity.
 	velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		$AnimatedSprite2D.play("jump")
-
-	# Get the input direction.
 	horizontal_movement()
 	#aplica movimento
 	move_and_slide()
 	#aplica animacao
-	if !Global.is_attacking:
+	if !Global.is_jumping and !Global.is_attacking:
 		player_animations()
 
